638bb2ae42791d4e53351d912d3046b9735a9084
Teodorovici Gavril-Anton <teodorovici.gavril11@gmail.com>
2021-03-12 23:07:12 +0200

    Add files via upload

---
+++ session1/test/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello from FII PRACTIC!!");
+    }
+}
